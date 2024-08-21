77ce2788afbc5e60ce506ae16ac00a055f084a53
phlucasfr <phlucasfr@gmail.com>
2024-08-21 11:43:18 -0300

    [FIX] - connection to redis

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -5,6 +5,10 @@ dotenv.config();
 
 const client = createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
 });
 
 client.on('error', (err) => {
