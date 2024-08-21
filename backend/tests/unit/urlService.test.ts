5046cf1e9d5daeac3bfbb1af48858057b637dcfc
phlucasfr <phlucasfr@gmail.com>
2024-08-21 12:22:49 -0300

    [FEAT] add testcontainers/redis to isolate tests

--- backend/tests/unit/urlService.test.ts
+++ backend/tests/unit/urlService.test.ts
@@ -1,9 +1,28 @@
 import { shortenUrlService, getUrlService } from '../../src/services/urlService';
 import { saveUrl, getUrl } from '../../src/repositories/urlRepository';
+import { RedisContainer, StartedRedisContainer } from '@testcontainers/redis';
+import { createClient, RedisClientType } from 'redis';
 
 jest.mock('../../src/repositories/urlRepository');
 
 describe('Url Service', () => {
+  let redisContainer: StartedRedisContainer;
+  let redisClient: RedisClientType;
+
+  beforeAll(async () => {
+    jest.setTimeout(60000);
+    redisContainer = await new RedisContainer().start();
+    redisClient = createClient({
+      url: redisContainer.getConnectionUrl(),
+    });
+    await redisClient.connect();
+  }, 60000);
+  
+  afterAll(async () => {
+    await redisClient.quit();
+    await redisContainer.stop();
+  }, 60000);  
+
   beforeEach(() => {
     jest.clearAllMocks();
   });
@@ -16,7 +35,7 @@ describe('Url Service', () => {
 
     const result = await shortenUrlService(originalUrl, 100);
 
-    expect(saveUrl).toHaveBeenCalledWith({originalUrl: originalUrl, shortId:result.shortId});
+    expect(saveUrl).toHaveBeenCalledWith({originalUrl: originalUrl, shortId: result.shortId});
     
     const gettedUrl = await getUrlService(result.shortUrl);
     expect(gettedUrl).toBe(originalUrl);
@@ -24,10 +43,10 @@ describe('Url Service', () => {
 
   it('deve retornar a URL original', async () => {
     const originalUrl = 'http://exampletest2.com';
-    const result = await shortenUrlService(originalUrl, 100);   
-
+    (saveUrl as jest.Mock).mockResolvedValue(undefined);
     (getUrl as jest.Mock).mockResolvedValue(originalUrl);
 
+    const result = await shortenUrlService(originalUrl, 100);
     const gettedUrl = await getUrlService(result.shortUrl);
     expect(gettedUrl).toBe(originalUrl);
   });
@@ -43,7 +62,7 @@ describe('Url Service', () => {
 
   it('deve tratar entradas maliciosas no retrieveUrl', async () => {
     const sqlInjectionAttempt = "' OR '1'='1";
- 
+
     (getUrl as jest.Mock).mockResolvedValue(null);
 
     const result = await getUrlService(sqlInjectionAttempt);
